a59e9b70e37ea3de9e79ce49956616e09b2d651f
Dan Costinas <danscostinas@gmail.com>
2023-12-25 17:16:36 +0200

    Add current  detection pipeline

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from kivy.graphics.texture import Texture
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.utils import platform
-import cv2
+import RubiksDetection.python_scripts.features as features
+import RubiksDetection.python_scripts.filtering as filtering
+import RubiksDetection.python_scripts.viewport_properties as viewport_properties
+import cv2 as cv
+import numpy as np
 import logging
 
 class KivyCamera(Image):
@@ -26,11 +30,23 @@ class KivyCamera(Image):
         if ret:
             #rotate 90 clockwise if on android
             if platform == 'android':
-                frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+                frame = cv.rotate(frame, cv.ROTATE_90_CLOCKWISE)
+
+            img = frame.copy()
+            frame = filtering.canny_amax_adaptive_filter(frame)
+            contours, hierarchy = cv.findContours(frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+            contours = features.contours_filter_small_area(contours, viewport_properties.FEATURES_FILTER_MIN_AREA)
+            contours = features.contours_filter_solidity(contours, viewport_properties.FEATURES_FILTER_SOLIDITY)
+            contours = features.contours_filter_positional_2(contours, viewport_properties.FEATURES_FILTER_POSITIONAL_2_DISTANCE)
+            contours = features.approx_polygon_from_contour(contours)
+            img_2 = cv.drawContours(img, contours, -1, (0,255,0), 3)
+
+
+            frame = img_2
             # convert frame to rgb
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
             # convert it to texture
-            buf1 = cv2.flip(frame, 0)
+            buf1 = cv.flip(frame, 0)
             buf= buf1.tostring()
             image_texture = Texture.create(
                 size=(frame.shape[1], frame.shape[0]), colorfmt='rgb')
@@ -43,10 +59,10 @@ class KivyCamera(Image):
 class CamApp(App):
     def build(self):
 
-        self.capture = cv2.VideoCapture(0)
-
+        self.capture = cv.VideoCapture(0)
         if not self.capture.isOpened():
             logging.fatal("Cam is not opened")
+        logging.info(f"Camera format is: {self.capture.get(cv.CAP_PROP_FORMAT)}")
         self.box_layout = BoxLayout(orientation='vertical')
 
         my_camera = KivyCamera(capture=self.capture, fps=30)
